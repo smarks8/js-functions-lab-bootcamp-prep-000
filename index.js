a2d7691bfd461a5ffaf9042cf2fe36eeb943ddac
stacey <staceyamarks@gmail.com>
2017-12-27 16:11:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
-function happyHolidayTo(name){
+function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
 }
